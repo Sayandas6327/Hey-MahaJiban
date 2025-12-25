e195fdb26c56275a61342f86bd1f5ebef3ac1842
Sayan Das <dassayan6327@gmail.com>
2025-12-25 16:37:20 +0530

    booksUplod cloudinaryStorage

--- Backend/middleware/booksUpload.js
+++ Backend/middleware/booksUpload.js
@@ -2,18 +2,41 @@
 const multer = require("multer");
 const fs = require("fs");
 //loading path
-const path = require("path");
+//const path = require("path");
+//loading cloudinary
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 // Ensure uploads folder exists
-const uploadPath = path.join(__dirname, `../public/books`);
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
+// const uploadPath = path.join(__dirname, `../public/books`);
+// if (!fs.existsSync(uploadPath)) {
+//   fs.mkdirSync(uploadPath, { recursive: true });
+// }
+
+//cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Multer storage config
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath);
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadPath);
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   },
+// });
+
+// Multer cloudinay storage config
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads",
+    resource_type: "auto",
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + "-" + file.originalname;
