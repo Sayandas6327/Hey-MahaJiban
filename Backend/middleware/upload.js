0852de53d842cb64c5c805ca9e41785a2b21b425
Sayan Das <dassayan6327@gmail.com>
2025-12-25 10:20:05 +0530

    cloudinary

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -2,25 +2,43 @@
 const multer = require("multer");
 const fs = require("fs");
 //loading path
-const path = require("path");
+// const path = require("path");
+//loading cloudinary
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 // Ensure uploads folder exists
-const uploadPath = path.join(__dirname, "../public/uploads");
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
+// const uploadPath = path.join(__dirname, "../public/uploads");
+// if (!fs.existsSync(uploadPath)) {
+//   fs.mkdirSync(uploadPath, { recursive: true });
+// }
 
-// Multer storage config
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
+//cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Multer storage config
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadPath);
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   },
+// });
+
+// Multer cloudinay storage config
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads",
+    resource_type: "auto",
+  },
+});
 // Optional file upload: allows requests without a file
 const upload = multer({ storage });
 
