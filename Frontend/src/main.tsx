aa14499a7b31560e6b2d61ad33b2123472d1a39b
Sayan Das <dassayan6327@gmail.com>
2026-01-19 19:22:17 +0530

    main tsx strictmode to browserrouter

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
+  
 )
